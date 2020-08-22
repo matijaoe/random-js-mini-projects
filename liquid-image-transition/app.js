ef651f09de4b144587191a96e646aeccbe1649f5
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-22 22:22:55 +0200

    added liquid-img-transition :wrench: :sparkles:

---
+++ liquid-image-transition/app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: 'img/bw1.jpg',
+    image2: 'img/bw2.jpg',
+    // angle: Math.PI / 4, // default
+    // speedIn: 2,
+    // speedOut: 2,
+    displacementImage: 'img/diss.png'
+})
